3fac66b306cfa21a91ae60c9c17e0be4e6d38511
mattsb42-aws <bullocm@amazon.com>
2017-12-04 11:40:38 -0800

    updating changelog and bumping to 1.3.3

--- src/aws_encryption_sdk/identifiers.py
+++ src/aws_encryption_sdk/identifiers.py
@@ -21,7 +21,7 @@ from cryptography.hazmat.primitives.kdf import hkdf
 
 from aws_encryption_sdk.exceptions import InvalidAlgorithmError
 
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 USER_AGENT_SUFFIX = 'AwsEncryptionSdkPython-KMSMasterKey/{}'.format(__version__)
 
 
