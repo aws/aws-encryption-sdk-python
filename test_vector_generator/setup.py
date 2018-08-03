4c47e138b85110118ebc2b427db7d56b8e42c032
mattsb42-aws <bullocm@amazon.com>
2018-08-03 00:35:01 -0700

    test vector handler package setup

--- test_vector_generator/setup.py
+++ test_vector_generator/setup.py
@@ -4,56 +4,59 @@ import re
 
 from setuptools import find_packages, setup
 
-VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+VERSION_RE = re.compile(r"""__version__ = ['"]([0-9.]+)['"]""")
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*args):
-    """Reads complete file contents."""
+    """Read complete file contents."""
     return open(os.path.join(HERE, *args)).read()
 
 
 def get_version():
-    """Reads the version from this module."""
-    init = read('src', 'aws_encryption_sdk', 'identifiers.py')
+    """Read the version from this module."""
+    init = read("src", "awses_test_vectors", "__init__.py")
     return VERSION_RE.search(init).group(1)
 
 
 def get_requirements():
-    """Reads the requirements file."""
-    requirements = read('requirements.txt')
+    """Read the requirements file."""
+    requirements = read("requirements.txt")
     return [r for r in requirements.strip().splitlines()]
 
 
 setup(
-    name='aws-encryption-sdk-vector-handlers',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    name="aws-encryption-sdk-vector-handlers",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     version=get_version(),
-    author='Amazon Web Services',
-    maintainer='Amazon Web Services',
-    author_email='aws-cryptools@amazon.com',
-    url='https://github.com/awslabs/aws-encryption-sdk-python',
-    description='Test vector handlers for the AWS Encryption SDK for Python',
-    long_description=read('README.rst'),
-    keywords='aws-encryption-sdk aws kms encryption',
-    license='Apache License 2.0',
+    author="Amazon Web Services",
+    maintainer="Amazon Web Services",
+    author_email="aws-cryptools@amazon.com",
+    url="https://github.com/awslabs/aws-encryption-sdk-python",
+    description="Test vector handlers for the AWS Encryption SDK for Python",
+    long_description=read("README.rst"),
+    keywords="aws-encryption-sdk aws kms encryption",
+    license="Apache License 2.0",
     install_requires=get_requirements(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Security',
-        'Topic :: Security :: Cryptography'
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+    ],
+    entry_points={
+        "console_scripts": ["awses-full-message-encrypt=awses_test_vectors.commands.full_message_encrypt:cli"]
+    },
 )
