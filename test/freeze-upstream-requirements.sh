c441c8f619bca009b68e68f23652ac47afe8c195
mattsb42-aws <bullocm@amazon.com>
2018-08-30 12:41:55 -0700

    Add: * upstream requirements freeze testenvs * frozen upstream requirements * upstream requirements tests

---
+++ test/freeze-upstream-requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Minimal wrapper script for upstream requirements install and freeze.
+# We do this here rather than as tox commands because tox does not support output redirection.
+if [ -z ${1} ]; then
+    exit 1
+fi
+
+pip install -r requirements.txt
+pip install -r test/requirements.txt
+pip freeze > ${1}
