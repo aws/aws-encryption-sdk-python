2d655e1e9e7b490a26dc85ec490e7ef3d7641b51
mattsb42-aws <bullocm@amazon.com>
2018-09-25 12:44:01 -0700

    switch to using the log handler provided in Chalice app

--- decrypt_oracle/src/aws_encryption_sdk_decryption_oracle/app.py
+++ decrypt_oracle/src/aws_encryption_sdk_decryption_oracle/app.py
@@ -13,7 +13,7 @@ from .key_providers.counting_master_key import CountingMasterKey
 from .key_providers.null_master_key import NullMasterKey
 
 APP = Chalice(app_name="aws-encryption-sdk-decryption-oracle")
-_LOGGER = logging.getLogger("aws-encryption-sdk-decryption-oracle")
+APP.log.setLevel(logging.DEBUG)
 
 try:  # Python 3.5.0 and 3.5.1 have incompatible typing modules
     from typing import Dict, Text, NoReturn, Union  # noqa pylint: disable=unused-import
@@ -35,8 +35,8 @@ def _master_key_provider():
 def basic_decrypt():
     # type: () -> Dict[Text, Union[Text, bool, int]]
     """Basic decrypt handler for decryption oracle v0."""
-    _LOGGER.debug("Request:")
-    _LOGGER.debug(json.dumps(APP.current_request.to_dict()))
+    APP.log.debug("Request:")
+    APP.log.debug(json.dumps(APP.current_request.to_dict()))
 
     try:
         request = APP.current_request.json_body
@@ -46,6 +46,6 @@ def basic_decrypt():
     except Exception as error:  # pylint: disable=broad-except
         response = {"body": str(error), "isBase64Encoded": False, "statusCode": 400}
 
-    _LOGGER.debug("Response:")
-    _LOGGER.debug(json.dumps(response))
+    APP.log.debug("Response:")
+    APP.log.debug(json.dumps(response))
     return response
