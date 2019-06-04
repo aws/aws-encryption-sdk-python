4be5bf86498fe190a79423f846dc92774c7a7901
Megha Vasant Shetty <mmegs@amazon.com>
2019-06-04 16:09:04 -0700

    Minor changes in some files

--- examples/test/test_i_basic_encryption.py
+++ examples/test/test_i_basic_encryption.py
@@ -11,8 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Unit test suite for the Strings examples in the AWS-hosted documentation."""
-import os
-
 import botocore.session
 import pytest
 
--- examples/test/test_i_one_kms_cmk.py
+++ examples/test/test_i_one_kms_cmk.py
@@ -11,7 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Unit test suite for the encryption and decryption using one KMS CMK example."""
-import os
 
 import botocore.session
 import pytest
