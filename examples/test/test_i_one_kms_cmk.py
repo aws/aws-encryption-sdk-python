1031f6e4ef61003cd57dd294801b77d92b99157f
MeghaShetty <mshetty@usc.edu>
2019-06-10 15:03:38 -0700

    Adding example one_kms_cmk (#158)
    * Adding example one_kms_cmk
    
    * Changes in one_kms_cmk and made random plaintext static
    
    * Changes in one_kms_cmk and made random plaintext static
    
    * Update one_kms_cmk.py
    
    * Changes in one_kms_cmk and made random plaintext static
    
    * Formatting changes in some files
    
    * Minor changes in some files
    
    * Adding example: Encryption decryption of streaming data using one KMS CMK
    
    * Added 'b' to the static plaintext
    
    * Wrapped static plaintext to <= 120 characters
    
    * Added docstrings
    
    * Corrected docstrings
    
    * Changed imports for same modules to be in the same statement and changed name of instance variable
    
    * Changed handle back

--- examples/test/test_i_one_kms_cmk.py
+++ examples/test/test_i_one_kms_cmk.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
@@ -10,18 +10,20 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""Unit test suite for the Strings examples in the AWS-hosted documentation."""
+"""Unit test suite for the encryption and decryption using one KMS CMK example."""
+
 import botocore.session
 import pytest
 
-from ..src.basic_encryption import cycle_string
-from .examples_test_utils import get_cmk_arn, static_plaintext
+from ..src.one_kms_cmk import encrypt_decrypt
+from .examples_test_utils import get_cmk_arn
+from .examples_test_utils import static_plaintext
 
 
 pytestmark = [pytest.mark.examples]
 
 
-def test_cycle_string():
+def test_one_kms_cmk():
     plaintext = static_plaintext
     cmk_arn = get_cmk_arn()
-    cycle_string(key_arn=cmk_arn, source_plaintext=plaintext, botocore_session=botocore.session.Session())
+    encrypt_decrypt(key_arn=cmk_arn, source_plaintext=plaintext, botocore_session=botocore.session.Session())
--- examples/test/test_i_one_kms_cmk_streaming_data.py
+++ examples/test/test_i_one_kms_cmk_streaming_data.py
@@ -10,28 +10,27 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""Unit test suite for the Bytes Streams Multiple Providers examples in the AWS-hosted documentation."""
+"""Unit test suite for the encryption and decryption of streaming data using one KMS CMK example."""
 import os
 import tempfile
 
 import botocore.session
 import pytest
 
-from ..src.basic_file_encryption_with_multiple_providers import cycle_file
-from .examples_test_utils import get_cmk_arn
-from .examples_test_utils import static_plaintext
+from ..src.one_kms_cmk_streaming_data import encrypt_decrypt_stream
+from .examples_test_utils import get_cmk_arn, static_plaintext
 
 
 pytestmark = [pytest.mark.examples]
 
 
-def test_cycle_file():
+def test_one_kms_cmk_streaming_data():
     cmk_arn = get_cmk_arn()
     handle, filename = tempfile.mkstemp()
     with open(filename, "wb") as f:
         f.write(static_plaintext)
     try:
-        new_files = cycle_file(
+        new_files = encrypt_decrypt_stream(
             key_arn=cmk_arn, source_plaintext_filename=filename, botocore_session=botocore.session.Session()
         )
         for f in new_files:
