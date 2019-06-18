c06b1fa695c753451b10e961382554f369dfb62b
Megha Vasant Shetty <mmegs@amazon.com>
2019-06-18 16:07:06 -0700

    Edited docstring again

--- src/aws_encryption_sdk/keyring/base.py
+++ src/aws_encryption_sdk/keyring/base.py
@@ -14,8 +14,7 @@
 
 
 class Keyring(object):
-    """Parent interface for Keyring classes.
-    """
+    """Parent interface for Keyring classes."""
 
     def on_encrypt(self, encryption_materials):
         """Generates a data key and encrypts it using all wrapping keys the Keyring is associated with.
