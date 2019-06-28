38c756be632f9f948a9039b73fbb056510e5b73c
Megha Vasant Shetty <mmegs@amazon.com>
2019-06-28 09:44:27 -0700

    Modified base

--- src/aws_encryption_sdk/keyring/base.py
+++ src/aws_encryption_sdk/keyring/base.py
@@ -14,7 +14,10 @@
 
 
 class Keyring(object):
-    """Parent interface for Keyring classes."""
+    """Parent interface for Keyring classes.
+
+    .. versionadded:: 1.5.0
+    """
 
     def on_encrypt(self, encryption_materials):
         """Generate a data key if not present and encrypt it using any available wrapping key.
@@ -22,19 +25,22 @@ class Keyring(object):
         :param encryption_materials: Contains signing key, encryption context and algorithm suite
                                     required to encrypt data key
         :type encryption_materials: aws_encryption_sdk.materials_managers.EncryptionMaterials
-        :returns: Contains signing key, unencrypted data key, encrypted data keys,
+        :returns: Encryption materials containing signing key, unencrypted data key, encrypted data keys,
                                     encryption context and algorithm suite required to encrypt data key
         :rtype: aws_encryption_sdk.materials_managers.EncryptionMaterials
         :raises NotImplementedError: if method is not implemented
         """
         raise NotImplementedError("Keyring does not implement on_encrypt function")
 
-    def on_decrypt(self, decryption_materials):
+    def on_decrypt(self, decryption_materials, encrypted_data_keys):
         """Attempt to decrypt the encrypted data keys.
 
-        :param decryption_materials: Contains verification key, list of encrypted data keys.
+        :param decryption_materials: May contain verification key, algorithm, encryption context and keyring trace.
         :type decryption_materials: aws_encryption_sdk.materials_managers.DecryptionMaterials
-        :returns: Contains verification key, list of encrypted data keys and decrypted data key.
+        :param encrypted_data_keys: List of encrypted data keys.
+        :type: Iterable of `aws_encryption_sdk.structures.EncryptedDataKey`
+        :returns: Decryption materials containing verification key, algorithm, data_encryption_key,
+                    encryption context and keyring trace.
         :rtype: aws_encryption_sdk.materials_managers.DecryptionMaterials
         :raises NotImplementedError: if method is not implemented
         """
