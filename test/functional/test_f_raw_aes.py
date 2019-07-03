8e7ff5a890619ee987686bdc16c6882415597f65
Megha Vasant Shetty <mmegs@amazon.com>
2019-07-03 08:59:22 -0700

    Saving changes to raw aes test

--- test/functional/test_f_raw_aes.py
+++ test/functional/test_f_raw_aes.py
@@ -19,7 +19,7 @@ from aws_encryption_sdk.keyring.raw_keyring import RawAESKeyring, WrappingKey
 
 pytestmark = [pytest.mark.functional, pytest.mark.local]
 
-_PLAINTEXT_DATA_KEY = b'*!\xa1"^-(\xf3\x105\x05i@B\xc2\xa2\xb7\xdd\xd5\xd5\xa9\xddm\xfae\xa8\\$\xf9d\x1e('
+_PLAINTEXT_DATA_KEY = [b'*!\xa1"^-(\xf3\x105\x05i@B\xc2\xa2\xb7\xdd\xd5\xd5\xa9\xddm\xfae\xa8\\$\xf9d\x1e(', ]
 _ENCRYPTION_CONTEXT = {"encryption": "context", "values": "here"}
 _PROVIDER_ID = "Random Raw Keys"
 _WRAPPING_KEY = b"\xeby-\x80A6\x15rA8\x83#,\xe4\xab\xac`\xaf\x99Z\xc1\xce\xdb\xb6\x0f\xb7\x805\xb2\x14J3"
@@ -27,14 +27,15 @@ _KEY_ID = b"5325b043-5843-4629-869c-64794af77ada"
 _KEY_INFO = b"5325b043-5843-4629-869c-64794af77ada\x00\x00\x00\x80\x00\x00\x00\x0c\xe0h\xe2NT\x1c\xb8\x8f!\t\xc2\x94"
 
 
-class RawAESEncryptionDecryption(RawAESKeyring):
+@pytest.mark.parametrize("plaintext_data_key, wrapping_key, key_info")
+def test_raw_aes_encryption_decryption(plaintext_data_key):
 
     key_namespace = _PROVIDER_ID
     key_name = _KEY_ID
     _wrapping_key = WrappingKey(wrapping_algorithm=WrappingAlgorithm.AES_256_GCM_IV12_TAG16_NO_PADDING,
                                 wrapping_key=_WRAPPING_KEY)
 
-
-
-
-
+    encryption_materials = RawAESKeyring.on_encrypt(encryption_materials=encryption_materials)
+    decryption_materials = RawAESKeyring.on_encrypt(decryption_materials=decryption_materials,
+                                                    encrypted_data_keys=encryption_materials.encrypted_data_keys)
+    assert encryption_materials.data_encryption_key == decryption_materials.data_key
