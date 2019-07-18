07d05f7ea4b60ce6a42a96319bca58f827f9f8cd
Megha Vasant Shetty <mmegs@amazon.com>
2019-07-18 14:23:12 -0700

    Added metaclass to base API and unit tests for base API

--- src/aws_encryption_sdk/keyring/base.py
+++ src/aws_encryption_sdk/keyring/base.py
@@ -11,6 +11,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Base class interface for Keyrings."""
+import abc
+
 from aws_encryption_sdk.materials_managers import DecryptionMaterials, EncryptionMaterials
 from aws_encryption_sdk.structures import EncryptedDataKey
 
@@ -21,12 +23,13 @@ except ImportError:  # pragma: no cover
     pass
 
 
-class Keyring(object):
+class Keyring(object, metaclass=abc.ABCMeta):
     """Parent interface for Keyring classes.
 
     .. versionadded:: 1.5.0
     """
 
+    @abc.abstractmethod
     def on_encrypt(self, encryption_materials):
         # type: (EncryptionMaterials) -> EncryptionMaterials
         """Generate a data key if not present and encrypt it using any available wrapping key.
@@ -39,6 +42,7 @@ class Keyring(object):
         """
         raise NotImplementedError("Keyring does not implement on_encrypt function")
 
+    @abc.abstractmethod
     def on_decrypt(self, decryption_materials, encrypted_data_keys):
         # type: (DecryptionMaterials, Iterable[EncryptedDataKey]) -> DecryptionMaterials
         """Attempt to decrypt the encrypted data keys.
---
+++ test/unit/test_base_keyring.py
@@ -0,0 +1,62 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Unit tests for base keyring."""
+
+import pytest
+
+from aws_encryption_sdk.identifiers import Algorithm
+from aws_encryption_sdk.keyring.base import Keyring
+from aws_encryption_sdk.keyring.raw_keyring import RawAESKeyring
+from aws_encryption_sdk.materials_managers import DecryptionMaterials, EncryptionMaterials
+
+try:  # Python 3.5.0 and 3.5.1 have incompatible typing modules
+    from typing import Iterable  # noqa pylint: disable=unused-import
+except ImportError:  # pragma: no cover
+    # We only actually need these imports when running the mypy checks
+    pass
+
+pytestmark = [pytest.mark.unit, pytest.mark.local]
+
+_encryption_materials = EncryptionMaterials(
+    algorithm=Algorithm.AES_256_GCM_IV12_TAG16_HKDF_SHA384_ECDSA_P384,
+    encryption_context={"encryption": "context", "values": "here"},
+    signing_key=b"aws-crypto-public-key",
+)
+
+_decryption_materials = DecryptionMaterials(
+    algorithm=Algorithm.AES_256_GCM_IV12_TAG16_HKDF_SHA384_ECDSA_P384, verification_key=b"ex_verification_key"
+)
+
+
+def test_keyring_no_encrypt():
+    class KeyringNoEncrypt(Keyring):
+        def on_decrypt(self, _decryption_materials, encrypted_data_keys):
+            # type: (DecryptionMaterials, Iterable[EncryptedDataKey]) -> DecryptionMaterials
+            decryption_materials = RawAESKeyring.on_decrypt(decryption_materials=_decryption_materials)
+            return decryption_materials
+
+    with pytest.raises(TypeError) as exc_info:
+        KeyringNoEncrypt()
+    exc_info.match("Can't instantiate abstract class KeyringNoEncrypt with abstract methods on_encrypt")
+
+
+def test_keyring_no_decrypt():
+    class KeyringNoDecrypt(Keyring):
+        def on_encrypt(self, _encryption_materials):
+            # type: (EncryptionMaterials) -> EncryptionMaterials
+            encryption_materials = RawAESKeyring.on_encrypt(encryption_materials=_encryption_materials)
+            return encryption_materials
+
+    with pytest.raises(TypeError) as exc_info:
+        KeyringNoDecrypt()
+    exc_info.match("Can't instantiate abstract class KeyringNoDecrypt with abstract methods on_decrypt")
