d3240ebcd383f99092573c4093d6c5c2bbc4be7b
Caitlin Tibbetts <tibbetc@amazon.com>
2019-07-26 13:22:41 -0700

    Formatting will be the death of me

--- examples/src/multiple_kms_cmk_regions.py
+++ examples/src/multiple_kms_cmk_regions.py
@@ -10,7 +10,10 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""Example showing basic encryption and decryption of a value already in memory using multiple KMS CMKs in multiple regions."""
+"""
+Example showing basic encryption and decryption of a value already in memory
+using multiple KMS CMKs in multiple regions.
+"""
 import aws_encryption_sdk
 
 
