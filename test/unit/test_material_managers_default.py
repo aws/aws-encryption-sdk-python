f31949a8322a6d505fa59fce6e8d222aab55bfb5
Adriano Hernandez <adrianoh@amazon.com>
2019-08-12 21:37:19 -0700

    This should fix formatting issues.

--- test/unit/test_material_managers_default.py
+++ test/unit/test_material_managers_default.py
@@ -11,7 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Test suite for aws_encryption_sdk.materials_managers.default"""
-import os
 
 import pytest
 from mock import MagicMock, sentinel
