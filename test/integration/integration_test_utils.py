c928c8dcb6d3f648b24a481af936c085b67ade66
Bohdan Kuzema <polamayster@gmail.com>
2019-09-20 15:19:18 +0300

    [issue-190] Creation of regional clients modifies default botocore session's region

--- test/integration/integration_test_utils.py
+++ test/integration/integration_test_utils.py
@@ -11,12 +11,14 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Utility functions to handle configuration and credentials setup for integration tests."""
+import botocore.session
 import os
 
 from aws_encryption_sdk.key_providers.kms import KMSMasterKeyProvider
 
 AWS_KMS_KEY_ID = "AWS_ENCRYPTION_SDK_PYTHON_INTEGRATION_TEST_AWS_KMS_KEY_ID"
 _KMS_MKP = None
+_KMS_MKP_BOTO = None
 
 
 def get_cmk_arn():
@@ -45,5 +47,21 @@ def setup_kms_master_key_provider(cache=True):
 
     if cache:
         _KMS_MKP = kms_master_key_provider
+
+    return kms_master_key_provider
+
+
+def setup_kms_master_key_provider_with_botocore_session(cache=True):
+    """Reads the test_values config file and builds the requested KMS Master Key Provider with botocore_session."""
+    global _KMS_MKP_BOTO  # pylint: disable=global-statement
+    if cache and _KMS_MKP_BOTO is not None:
+        return _KMS_MKP_BOTO
+
+    cmk_arn = get_cmk_arn()
+    kms_master_key_provider = KMSMasterKeyProvider(botocore_session=botocore.session.Session())
+    kms_master_key_provider.add_master_key(cmk_arn)
+
+    if cache:
+        _KMS_MKP_BOTO = kms_master_key_provider
 
     return kms_master_key_provider
