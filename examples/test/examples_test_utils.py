8026e3173c92ecb33a1514b7dd29e714ba81df5c
mattsb42-aws <bullocm@amazon.com>
2020-03-07 12:46:36 -0800

    chore: add automatic test runner for examples

--- examples/test/examples_test_utils.py
+++ examples/test/examples_test_utils.py
@@ -1,18 +1,21 @@
-# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Helper utilities for use while testing examples."""
 import os
 import sys
+import inspect
+
+import pytest
+import six
+
+try:  # Python 3.5.0 and 3.5.1 have incompatible typing modules
+    from typing import Callable, List, Iterable  # noqa pylint: disable=unused-import
+except ImportError:  # pragma: no cover
+    # We only actually need these imports when running the mypy checks
+    pass
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+EXAMPLES_SOURCE = os.path.join(HERE, "..", "src")
 
 os.environ["AWS_ENCRYPTION_SDK_EXAMPLES_TESTING"] = "yes"
 sys.path.extend([os.sep.join([os.path.dirname(__file__), "..", "..", "test", "integration"])])
@@ -48,3 +51,39 @@ static_plaintext = (
 
 
 from integration_test_utils import get_cmk_arn  # noqa pylint: disable=unused-import,import-error
+
+
+@pytest.fixture
+def aws_kms_cmk_arns():
+    # type: () -> List[str]
+    return [get_cmk_arn()]
+
+
+def all_examples():
+    # type: () -> Iterable[pytest.param]
+    for (dirpath, dirnames, filenames) in os.walk(EXAMPLES_SOURCE):
+        for testfile in filenames:
+            split_path = testfile.rsplit(".", 1)
+            if len(split_path) != 2:
+                continue
+            stem, suffix = split_path
+            if suffix == "py" and stem != "__init__":
+                module_parent = dirpath[len(EXAMPLES_SOURCE) + 1 :].replace("/", ".")
+                module_name = stem
+                if module_parent:
+                    import_path = "..src.{base}.{name}".format(base=module_parent, name=module_name)
+                else:
+                    import_path = "..src.{name}".format(name=module_name)
+
+                yield pytest.param(import_path, id="{base}.{name}".format(base=module_parent, name=module_name))
+
+
+def get_arg_names(function):
+    # type: (Callable) -> List[str]
+    if six.PY2:
+        # getargspec was deprecated in CPython 3.0 but 2.7 does not have either of the new options
+        spec = inspect.getargspec(function)
+        return spec.args
+
+    spec = inspect.getfullargspec(function)
+    return spec.args
---
+++ examples/test/test_run_examples.py
@@ -0,0 +1,40 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""Test all examples."""
+from importlib import import_module
+
+import pytest
+
+from .examples_test_utils import all_examples, aws_kms_cmk_arns, get_arg_names, static_plaintext
+
+pytestmark = [pytest.mark.examples]
+SINGLE_CMK_ARG = "aws_kms_cmk_arn"
+GENERATOR_CMK_ARG = "aws_kms_generator_cmk"
+CHILD_CMK_ARG = "aws_kms_child_cmks"
+PLAINTEXT_ARG = "plaintext"
+
+
+@pytest.mark.parametrize("import_path", all_examples())
+def test_examples(import_path, aws_kms_cmk_arns):
+    module = import_module(name=import_path, package=__package__)
+    try:
+        run_function = getattr(module, "run")
+    except AttributeError:
+        pytest.skip("Module lacks 'run' function.")
+        return
+
+    args = get_arg_names(run_function)
+    possible_kwargs = {
+        SINGLE_CMK_ARG: aws_kms_cmk_arns[0],
+        GENERATOR_CMK_ARG: aws_kms_cmk_arns[0],
+        CHILD_CMK_ARG: aws_kms_cmk_arns[1:],
+        PLAINTEXT_ARG: static_plaintext,
+    }
+    kwargs = {}
+    for name in args:
+        try:
+            kwargs[name] = possible_kwargs[name]
+        except KeyError:
+            pass
+
+    run_function(**kwargs)
