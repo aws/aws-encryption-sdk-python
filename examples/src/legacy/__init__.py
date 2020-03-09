78484eb12283e12e90125148d6fadc879446e33f
mattsb42-aws <bullocm@amazon.com>
2020-03-08 17:32:01 -0700

    docs: add docstring description for legacy examples module

--- examples/src/legacy/__init__.py
+++ examples/src/legacy/__init__.py
@@ -1,2 +1,11 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+"""
+Legacy examples.
+
+These are any examples that were already defined
+before we started revamping our examples.
+We are keeping them around for anyone who needs them as reference material,
+but we recommend looking at the newer examples
+that should provide a clearer picture of how to use this library.
+"""
