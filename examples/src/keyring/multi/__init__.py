0047ddf2137a1ba8cb75f4a2d21562e2a7884328
mattsb42-aws <bullocm@amazon.com>
2020-03-11 12:08:43 -0700

    docs: add multi-keyring example

---
+++ examples/src/keyring/multi/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Multi-keyring examples.
+
+These examples show how to use the multi-keyring.
+"""
