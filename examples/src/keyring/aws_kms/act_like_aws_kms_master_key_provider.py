b3598d339a9ea0b954da4c88d4742a4092c05474
mattsb42-aws <bullocm@amazon.com>
2020-04-22 12:53:35 -0700

    docs: revise AWS KMS keyring-as-mkp example * show using multiple CMKs * demonstrate how the generating CMK is defined in both * explain that it does not matter if the CMK keyring fails

--- examples/src/keyring/aws_kms/act_like_aws_kms_master_key_provider.py
+++ examples/src/keyring/aws_kms/act_like_aws_kms_master_key_provider.py
@@ -30,12 +30,19 @@ from aws_encryption_sdk.key_providers.kms import KMSMasterKeyProvider
 from aws_encryption_sdk.keyrings.aws_kms import AwsKmsKeyring
 from aws_encryption_sdk.keyrings.multi import MultiKeyring
 
+try:  # Python 3.5.0 and 3.5.1 have incompatible typing modules
+    from typing import Sequence  # noqa pylint: disable=unused-import
+except ImportError:  # pragma: no cover
+    # We only actually need these imports when running the mypy checks
+    pass
 
-def run(aws_kms_cmk, source_plaintext):
-    # type: (str, bytes) -> None
+
+def run(aws_kms_cmk, aws_kms_additional_cmks, source_plaintext):
+    # type: (str, Sequence[str], bytes) -> None
     """Demonstrate how to create a keyring that behaves like an AWS KMS master key provider.
 
     :param str aws_kms_cmk: The ARN of an AWS KMS CMK that protects data keys
+    :param List[str] aws_kms_additional_cmks: Additional ARNs of secondary AWS KMS CMKs
     :param bytes source_plaintext: Plaintext to encrypt
     """
     # Prepare your encryption context.
@@ -50,13 +57,22 @@ def run(aws_kms_cmk, source_plaintext):
 
     # This is the master key provider whose behavior we want to reproduce.
     #
-    # When encrypting, this master key provider uses only the specified `aws_kms_cmk`.
+    # When encrypting, this master key provider generates the data key using the first CMK in the list
+    # and encrypts the data key using all specified CMKs.
     # However, when decrypting, this master key provider attempts to decrypt
     # any data keys that were encrypted under an AWS KMS CMK.
-    _master_key_provider_to_replicate = KMSMasterKeyProvider(key_ids=[aws_kms_cmk])  # noqa: intentionally never used
+    master_key_provider_cmks = [aws_kms_cmk] + aws_kms_additional_cmks
+    _master_key_provider_to_replicate = KMSMasterKeyProvider(  # noqa: intentionally never used
+        key_ids=master_key_provider_cmks,
+    )
 
-    # Create a single-CMK keyring that encrypts and decrypts using a single AWS KMS CMK.
-    single_cmk_keyring = AwsKmsKeyring(generator_key_id=aws_kms_cmk)
+    # Create a CMK keyring that encrypts and decrypts using the specified AWS KMS CMKs.
+    #
+    # This keyring reproduces the encryption behavior of the AWS KMS master key provider.
+    #
+    # The AWS KMS keyring requires that you explicitly identify the CMK
+    # that you want the keyring to use to generate the data key.
+    cmk_keyring = AwsKmsKeyring(generator_key_id=aws_kms_cmk, key_ids=aws_kms_additional_cmks)
 
     # Create an AWS KMS discovery keyring that will attempt to decrypt
     # any data keys that were encrypted under an AWS KMS CMK.
@@ -64,7 +80,15 @@ def run(aws_kms_cmk, source_plaintext):
 
     # Combine the single-CMK and discovery keyrings
     # to create a keyring that behaves like an AWS KMS master key provider.
-    keyring = MultiKeyring(generator=single_cmk_keyring, children=[discovery_keyring])
+    #
+    # The CMK keyring reproduces the encryption behavior
+    # and the discovery keyring reproduces the decryption behavior.
+    # This also means that it does not matter if the CMK keyring fails on decrypt,
+    # for example if you configured it with aliases which would work on encrypt
+    # but fail to match any encrypted data keys on decrypt,
+    # because the discovery keyring attempts to decrypt any AWS KMS-encrypted
+    # data keys that it finds.
+    keyring = MultiKeyring(generator=cmk_keyring, children=[discovery_keyring])
 
     # Encrypt your plaintext data.
     ciphertext, _encrypt_header = aws_encryption_sdk.encrypt(
