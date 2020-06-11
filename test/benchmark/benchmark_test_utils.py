bcf47237957a7c79370ce78b3e91ebec5e477010
mattsb42-aws <bullocm@amazon.com>
2020-06-11 16:19:54 -0700

    chore: reorganize benchmark files into their own directory and specifically only mark KMS tests as integ

--- test/benchmark/benchmark_test_utils.py
+++ test/benchmark/benchmark_test_utils.py
@@ -1,25 +1,17 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-"""Functional performance test suite for ``aws_encryption_sdk``."""
+"""Helper utilities for benchmark tests."""
 import copy
 
 import pytest
 
 import aws_encryption_sdk
 from aws_encryption_sdk.caches.local import LocalCryptoMaterialsCache
+from aws_encryption_sdk.identifiers import AlgorithmSuite
 from aws_encryption_sdk.keyrings.base import Keyring
 from aws_encryption_sdk.materials_managers.caching import CachingCryptoMaterialsManager
 from aws_encryption_sdk.materials_managers.default import DefaultCryptoMaterialsManager
 
-from ..unit.unit_test_utils import (
-    ephemeral_raw_aes_keyring,
-    ephemeral_raw_aes_master_key,
-    ephemeral_raw_rsa_keyring,
-    ephemeral_raw_rsa_master_key,
-)
-from .integration_test_utils import build_aws_kms_keyring, setup_kms_master_key_provider
-
-pytestmark = [pytest.mark.benchmark]
 ENCRYPTION_CONTEXT = {
     "encryption": "context",
     "is not": "secret",
@@ -29,10 +21,21 @@ ENCRYPTION_CONTEXT = {
 }
 
 
-def _cycle(**kwargs):
+def all_operations():
+    return pytest.mark.parametrize(
+        "operation",
+        (
+            pytest.param(aws_encryption_sdk.encrypt, id="encrypt only"),
+            pytest.param(aws_encryption_sdk.decrypt, id="decrypt only"),
+            pytest.param(encrypt_decrypt_cycle, id="encrypt decrypt cycle"),
+        ),
+    )
+
+
+def encrypt_decrypt_cycle(**kwargs):
     encrypt_kwargs = copy.copy(kwargs)
     decrypt_kwargs = copy.copy(kwargs)
-    for param in ("encryption_context", "frame_length", "source"):
+    for param in ("encryption_context", "frame_length", "source", "algorithm"):
         try:
             del decrypt_kwargs[param]
         except KeyError:
@@ -43,35 +46,7 @@ def _cycle(**kwargs):
     aws_encryption_sdk.decrypt(**decrypt_kwargs)
 
 
-@pytest.mark.parametrize(
-    "provider_builder",
-    (
-        pytest.param(ephemeral_raw_aes_keyring, id="Raw AES keyring"),
-        pytest.param(ephemeral_raw_aes_master_key, id="Raw AES master key"),
-        pytest.param(ephemeral_raw_rsa_keyring, id="Raw RSA keyring"),
-        pytest.param(ephemeral_raw_rsa_master_key, id="Raw RSA master key"),
-        pytest.param(build_aws_kms_keyring, id="AWS KMS keyring"),
-        pytest.param(setup_kms_master_key_provider, id="AWS KMS master key provider"),
-    ),
-)
-@pytest.mark.parametrize(
-    "cache_messages",
-    (
-        pytest.param(0, id="no cache"),
-        pytest.param(1000000, id="cache and only miss once"),
-        pytest.param(10, id="cache and miss every 10"),
-    ),
-)
-@pytest.mark.parametrize("plaintext, frame_length", (pytest.param("foo", 1024, id="single frame"),))
-@pytest.mark.parametrize(
-    "operation",
-    (
-        pytest.param(aws_encryption_sdk.encrypt, id="encrypt only"),
-        pytest.param(aws_encryption_sdk.decrypt, id="decrypt only"),
-        pytest.param(_cycle, id="encrypt decrypt cycle"),
-    ),
-)
-def test_end2end_performance(benchmark, provider_builder, cache_messages, plaintext, frame_length, operation):
+def build_cmm(provider_builder, cache_messages):
     provider = provider_builder()
     if isinstance(provider, Keyring):
         provider_param = "keyring"
@@ -88,12 +63,27 @@ def test_end2end_performance(benchmark, provider_builder, cache_messages, plaint
             **{provider_param: provider}
         )
 
+    return cmm
+
+
+def run_benchmark(
+    benchmark,
+    provider_builder,
+    operation,
+    cache_messages=0,
+    plaintext="foo",
+    frame_length=1024,
+    algorithm=AlgorithmSuite.AES_256_GCM_IV12_TAG16_HKDF_SHA384_ECDSA_P384,
+):
+    cmm = build_cmm(provider_builder, cache_messages)
+
     kwargs = dict(
         source=plaintext,
         materials_manager=cmm,
         encryption_context=copy.copy(ENCRYPTION_CONTEXT),
         frame_length=frame_length,
+        algorithm=algorithm,
     )
     if operation is aws_encryption_sdk.decrypt:
         kwargs = dict(source=aws_encryption_sdk.encrypt(**kwargs).result, materials_manager=cmm,)
-    benchmark.pedantic(target=operation, kwargs=kwargs, iterations=100, rounds=10)
+    benchmark.pedantic(target=operation, kwargs=kwargs, iterations=10, rounds=10)
