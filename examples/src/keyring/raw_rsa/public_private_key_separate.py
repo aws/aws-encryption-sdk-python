0c453806f414f3e6b6d672aa731f5c9dbb247597
Matthew Jones <mmtj@amazon.com>
2020-08-13 10:23:33 -0700

    refactor: Update missed lines in enxamples

--- examples/src/keyring/raw_rsa/public_private_key_separate.py
+++ examples/src/keyring/raw_rsa/public_private_key_separate.py
@@ -83,7 +83,7 @@ def run(source_plaintext):
     private_key_keyring = RawRSAKeyring(
         # The key namespace and key name MUST match the encrypt keyring.
         key_namespace="some managed raw keys",
-        key_name=b"my RSA wrapping key",
+        key_name="my RSA wrapping key",
         private_wrapping_key=private_key,
         # The wrapping algorithm MUST match the encrypt keyring.
         wrapping_algorithm=WrappingAlgorithm.RSA_OAEP_SHA256_MGF1,
