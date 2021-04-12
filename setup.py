d3c7aea0ab0018ab83eff25cd123b58de5ab6eef
Ben Farley <47006790+farleyb-amazon@users.noreply.github.com>
2021-04-12 12:42:12 -0600

    chore: Add py38 and p39 to CI (#329)

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Security",
         "Topic :: Security :: Cryptography",
