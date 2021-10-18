8ef7e5ff849095fc826b275e1c1e77accc74be32
Jose Corella <jocorell@amazon.com>
2021-10-18 14:09:43 -0700

    add back test vector handler for requirements

--- test_vector_handlers/setup.py
+++ test_vector_handlers/setup.py
@@ -45,6 +45,7 @@ setup(
         "Natural Language :: English",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
