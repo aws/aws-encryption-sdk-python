48157378a1a61ba272bd43bd137d8a884a53f25f
Jose Corella <jocorell@amazon.com>
2022-06-07 10:58:29 -0700

    refactor: change verify_interface to isinstance

--- src/aws_encryption_sdk/internal/crypto/authentication.py
+++ src/aws_encryption_sdk/internal/crypto/authentication.py
@@ -18,7 +18,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric.utils import Prehashed
-from cryptography.utils import InterfaceNotImplemented, verify_interface
 
 from ...exceptions import NotSupportedError
 from .elliptic_curve import (
@@ -47,11 +46,10 @@ class _PrehashingAuthenticator(object):
 
     def _set_signature_type(self):
         """Ensures that the algorithm signature type is a known type and sets a reference value."""
-        try:
-            verify_interface(ec.EllipticCurve, self.algorithm.signing_algorithm_info)
-            return ec.EllipticCurve
-        except InterfaceNotImplemented:
-            raise NotSupportedError("Unsupported signing algorithm info")
+        for method in ec.EllipticCurve.__abstractmethods__:
+            if not hasattr(self.algorithm.signing_algorithm_info(), method):
+                raise NotSupportedError("Unsupported signing algorithm info")
+        return ec.EllipticCurve
 
     def _build_hasher(self):
         """Builds the hasher instance which will calculate the digest of all passed data.
