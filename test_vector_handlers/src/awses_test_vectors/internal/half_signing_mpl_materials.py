a62f1b4d2a1c824da5796409d93e43b6fcb03075
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-03-13 18:18:19 -0700

    debug cb

--- test_vector_handlers/src/awses_test_vectors/internal/half_signing_mpl_materials.py
+++ test_vector_handlers/src/awses_test_vectors/internal/half_signing_mpl_materials.py
@@ -15,16 +15,21 @@ class HalfSigningEncryptionMaterialsFromMPL(EncryptionMaterialsFromMPL):
     This is used in testing malicious message modification (HalfSigningTampering).
     """
 
+    _underlying_materials: EncryptionMaterialsFromMPL
+
+    def __init__(self, underling_materials):
+        self._underlying_materials = underling_materials
+
     # pylint thinks EncryptionMaterialsFromMPL.algorithm is a method
     # pylint: disable=invalid-overridden-method
-    @EncryptionMaterialsFromMPL.algorithm.getter
+    @property
     def algorithm(self):
         """Return any previously-provided overriden algorithm;
         if none was provided, returns underlying algorithm from encryption materials.
         """
         if hasattr(self, "set_algorithm"):
             return self.set_algorithm
-        return self.algorithm
+        return self._underlying_materials.algorithm
 
     @algorithm.setter
     def algorithm(self, algorithm):
@@ -32,15 +37,31 @@ class HalfSigningEncryptionMaterialsFromMPL(EncryptionMaterialsFromMPL):
 
     # pylint thinks EncryptionMaterialsFromMPL.signing_key is a method
     # pylint: disable=invalid-overridden-method
-    @EncryptionMaterialsFromMPL.signing_key.getter
+    @property
     def signing_key(self):
         """Return any previously-provided overriden signing_key;
         if none was provided, returns underlying signing_key from encryption materials.
         """
         if hasattr(self, "set_signing_key"):
             return self.set_signing_key
-        return self.signing_key
+        return self._underlying_materials.algorithm
 
     @signing_key.setter
     def signing_key(self, signing_key):
         self.set_signing_key = signing_key
+
+    @property
+    def encryption_context(self):
+        return self._underlying_materials.encryption_context
+
+    @property
+    def encrypted_data_keys(self):
+        return self._underlying_materials.encrypted_data_keys
+    
+    @property
+    def data_encryption_key(self):
+        return self._underlying_materials.data_encryption_key
+    
+    @property
+    def required_encryption_context_keys(self):
+        return self._underlying_materials.required_encryption_context_keys
