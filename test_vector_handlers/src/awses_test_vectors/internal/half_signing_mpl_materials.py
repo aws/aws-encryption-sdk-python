e65fec4878d141ee18286acc74ae4c387f699182
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-03-13 17:16:17 -0700

    debug gha

--- test_vector_handlers/src/awses_test_vectors/internal/half_signing_mpl_materials.py
+++ test_vector_handlers/src/awses_test_vectors/internal/half_signing_mpl_materials.py
@@ -2,6 +2,8 @@
 This must ONLY be used in testing and NOT in production..
 This is used in testing malicious message modification (HalfSigningTampering).
 """
+# Ignore missing MPL for pylint, but the MPL is required for this class
+# pylint: disable=import-error,no-name-in-module
 from aws_encryption_sdk.materials_managers.mpl.materials import (
     EncryptionMaterialsFromMPL
 )
@@ -16,6 +18,9 @@ class HalfSigningEncryptionMaterialsFromMPL(EncryptionMaterialsFromMPL):
     # pylint: disable=invalid-overridden-method
     @EncryptionMaterialsFromMPL.algorithm.getter
     def algorithm(self):
+        """Returns any previously-provided overriden algorithm;
+        if none was provided, returns underlying algorithm from encryption materials.
+        """
         if hasattr(self, "set_algorithm"):
             return self.set_algorithm
         return self.algorithm
@@ -28,6 +33,9 @@ class HalfSigningEncryptionMaterialsFromMPL(EncryptionMaterialsFromMPL):
     # pylint: disable=invalid-overridden-method
     @EncryptionMaterialsFromMPL.signing_key.getter
     def signing_key(self):
+        """Returns any previously-provided overriden signing_key;
+        if none was provided, returns underlying signing_key from encryption materials.
+        """
         if hasattr(self, "set_signing_key"):
             return self.set_signing_key
         return self.signing_key
