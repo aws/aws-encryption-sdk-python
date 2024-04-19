08a80a3f96bf7a42be9d771bb549c353b7a45b06
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-04-19 09:04:31 -0700

    fix

--- src/aws_encryption_sdk/internal/deprecation.py
+++ src/aws_encryption_sdk/internal/deprecation.py
@@ -3,15 +3,21 @@ import functools
 
 def deprecated(reason):
     def decorator(cls):
-        original_init = cls.__init__  # Save the original __init__
+        if cls.__init__ is object.__init__:
+            # If cls.__init__ is object.__init__, define a basic __init__ to wrap
+            def new_init(self, *args, **kwargs):
+                warnings.warn(f"{cls.__name__} is deprecated: {reason}",
+                              category=DeprecationWarning, stacklevel=2)
+        else:
+            original_init = cls.__init__  # Save the original __init__
 
-        @functools.wraps(cls.__init__)
-        def new_init(self, *args, **kwargs):
-            # Emit the deprecation warning whenever the class is instantiated
-            warnings.warn(f"{cls.__name__} is deprecated: {reason}",
-                          category=DeprecationWarning, stacklevel=2)
-            original_init(self, *args, **kwargs)  # Call the original __init__
+            @functools.wraps(cls.__init__)
+            def new_init(self, *args, **kwargs):
+                warnings.warn(f"{cls.__name__} is deprecated: {reason}",
+                              category=DeprecationWarning, stacklevel=2)
+                original_init(self, *args, **kwargs)  # Call the original __init__
 
         cls.__init__ = new_init
         return cls
+    
     return decorator
