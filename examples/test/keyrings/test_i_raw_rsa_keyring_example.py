6b88b54844949ba4f2fc034047cec2a174f6d07c
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-04-23 16:47:58 -0700

    updated test_i_raw_rsa_keyring_example.py

--- examples/test/keyrings/test_i_raw_rsa_keyring_example.py
+++ examples/test/keyrings/test_i_raw_rsa_keyring_example.py
@@ -1,6 +1,6 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-"""Test suite for the Raw AES keyring example."""
+"""Test suite for the Raw RSA keyring example."""
 import pytest
 
 from ...src.keyrings.raw_rsa_keyring_example import encrypt_and_decrypt_with_keyring
@@ -9,5 +9,5 @@ pytestmark = [pytest.mark.examples]
 
 
 def test_encrypt_and_decrypt_with_keyring():
-    """Test function for encrypt and decrypt using the Raw AES Keyring example."""
+    """Test function for encrypt and decrypt using the Raw RSA Keyring example."""
     encrypt_and_decrypt_with_keyring()
