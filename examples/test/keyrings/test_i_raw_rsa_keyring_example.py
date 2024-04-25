f6e53e2ac610175fd17f7b1e4a8530b8b6ee07ef
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-04-25 14:50:00 -0700

    minor fix

--- examples/test/keyrings/test_i_raw_rsa_keyring_example.py
+++ examples/test/keyrings/test_i_raw_rsa_keyring_example.py
@@ -10,14 +10,16 @@ pytestmark = [pytest.mark.examples]
 
 def test_encrypt_and_decrypt_with_keyring_without_user_defined_keys():
     """Test function for encrypt and decrypt using the Raw RSA Keyring example
-    where user doesn't provide the public and private keys"""
+    where user doesn't provide the public and private keys
+    """
     encrypt_and_decrypt_with_keyring()
 
 
 def test_encrypt_and_decrypt_with_keyring_with_user_defined_keys():
     """Test function for encrypt and decrypt using the Raw RSA Keyring example
     where user provides the public and private keys. To test this, we create the
-    keys using the generate_rsa_keys function"""
+    keys using the generate_rsa_keys function
+    """
     user_public_key, user_private_key = generate_rsa_keys()
     encrypt_and_decrypt_with_keyring(public_key=user_public_key, private_key=user_private_key)
 
@@ -26,7 +28,8 @@ def test_encrypt_and_decrypt_fails_if_user_provides_only_public_key():
     """Test function for encrypt and decrypt using the Raw RSA Keyring example
     where user provides only the public key. The program should throw an Assertion error
     as this example requires the user to either provide both private and public keys to
-    test both encryption and decryption, or not provide any keys and the example generates both"""
+    test both encryption and decryption, or not provide any keys and the example generates both
+    """
     user_public_key, user_private_key = generate_rsa_keys()
     try:
         encrypt_and_decrypt_with_keyring(public_key=user_public_key)
@@ -40,7 +43,8 @@ def test_encrypt_and_decrypt_fails_if_user_provides_only_private_key():
     """Test function for encrypt and decrypt using the Raw RSA Keyring example
     where user provides only the private key. The program should throw an Assertion error
     as this example requires the user to either provide both private and public keys to
-    test both encryption and decryption, or not provide any keys and the example generates both"""
+    test both encryption and decryption, or not provide any keys and the example generates both
+    """
     user_public_key, user_private_key = generate_rsa_keys()
     try:
         encrypt_and_decrypt_with_keyring(private_key=user_private_key)
