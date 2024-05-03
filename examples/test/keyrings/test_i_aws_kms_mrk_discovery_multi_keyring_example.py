b4912949fffacc2776f00efd87e2daef1b95329f
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-05-03 16:21:56 -0700

    fix flake8

--- examples/test/keyrings/test_i_aws_kms_mrk_discovery_multi_keyring_example.py
+++ examples/test/keyrings/test_i_aws_kms_mrk_discovery_multi_keyring_example.py
@@ -9,9 +9,7 @@ pytestmark = [pytest.mark.examples]
 
 
 def test_encrypt_and_decrypt_with_keyring():
-    """Test function for encrypt and decrypt using the
-    AWS KMS MRK Discovery Multi Keyring example.
-    """
+    """Test function for encrypt and decrypt using AWS KMS MRK Discovery Multi Keyring example."""
     mrk_key_id_encrypt = \
         "arn:aws:kms:us-east-1:658956600833:key/mrk-80bd8ecdcd4342aebd84b7dc9da498a7"
     mrk_encrypt_region = "us-east-1"
