048a7993796a192b4090f2cfd0dc1a87a0d38ad5
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-05-06 10:01:36 -0700

    first commit

---
+++ examples/test/keyrings/test_i_set_encryption_algorithm_example.py
@@ -0,0 +1,13 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""Test suite for the Set Encryption Algorithm example for a Raw AES keyring."""
+import pytest
+
+from ...src.keyrings.set_encryption_algorithm_example import encrypt_and_decrypt_with_keyring
+
+pytestmark = [pytest.mark.examples]
+
+
+def test_encrypt_and_decrypt_with_keyring():
+    """Test function for setting an encryption algorithm in a Raw AES Keyring."""
+    encrypt_and_decrypt_with_keyring()
