d8e20f12e22420e69619215e157e4909ecaec37a
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-05-06 14:20:01 -0700

    fix flake8; gitignore

--- examples/src/keyrings/file_streaming_example.py
+++ examples/src/keyrings/file_streaming_example.py
@@ -24,9 +24,9 @@ but you can include multiple Raw AES keyrings, alone or with other keyrings, in 
 For more information on how to use Raw AES keyrings, see
 https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/use-raw-aes-keyring.html
 """
+import filecmp
 import secrets
 import sys
-import filecmp
 
 from aws_cryptographic_materialproviders.mpl import AwsCryptographicMaterialProviders
 from aws_cryptographic_materialproviders.mpl.config import MaterialProvidersConfig
