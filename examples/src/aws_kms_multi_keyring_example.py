70d1364663aa1c7241ba96506b3d6eea865ea82b
Ritvik Kapila <61410899+RitvikKapila@users.noreply.github.com>
2024-05-10 10:52:40 -0700

    chore(keyring examples): refactor keyring examples and move master key provider examples to legacy (#678)

---
+++ examples/src/aws_kms_multi_keyring_example.py
@@ -0,0 +1,207 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+This example sets up the AWS KMS Multi Keyring made up of multiple AWS KMS Keyrings.
+
+A multi-keyring is a keyring that consists of one or more individual keyrings of the
+same or a different type. The effect is like using several keyrings in a series.
+When you use a multi-keyring to encrypt data, any of the wrapping keys in any of its
+keyrings can decrypt that data.
+
+When you create a multi-keyring to encrypt data, you designate one of the keyrings as
+the generator keyring. All other keyrings are known as child keyrings. The generator keyring
+generates and encrypts the plaintext data key. Then, all of the wrapping keys in all of the
+child keyrings encrypt the same plaintext data key. The multi-keyring returns the plaintext
+key and one encrypted data key for each wrapping key in the multi-keyring. If you create a
+multi-keyring with no generator keyring, you can use it to decrypt data, but not to encrypt.
+If the generator keyring is a KMS keyring, the generator key in the AWS KMS keyring generates
+and encrypts the plaintext key. Then, all additional AWS KMS keys in the AWS KMS keyring,
+and all wrapping keys in all child keyrings in the multi-keyring, encrypt the same plaintext key.
+
+When decrypting, the AWS Encryption SDK uses the keyrings to try to decrypt one of the encrypted
+data keys. The keyrings are called in the order that they are specified in the multi-keyring.
+Processing stops as soon as any key in any keyring can decrypt an encrypted data key.
+
+This example creates a Multi Keyring and then encrypts a custom input EXAMPLE_DATA
+with an encryption context. This example also includes some sanity checks for demonstration:
+1. Ciphertext and plaintext data are not the same
+2. Decryption of ciphertext is possible using the multi_keyring,
+   and every one of the keyrings from the multi_keyring separately
+3. All decrypted plaintext value match EXAMPLE_DATA
+These sanity checks are for demonstration in the example only. You do not need these in your code.
+
+This example creates a multi_keyring using a KMS keyring as generator keyring and
+another KMS keyring as a child keyring.
+
+For more information on how to use Multi keyrings, see
+https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/use-multi-keyring.html
+"""
+import sys
+
+import boto3
+from aws_cryptographic_materialproviders.mpl import AwsCryptographicMaterialProviders
+from aws_cryptographic_materialproviders.mpl.config import MaterialProvidersConfig
+from aws_cryptographic_materialproviders.mpl.models import CreateAwsKmsKeyringInput, CreateAwsKmsMultiKeyringInput
+from aws_cryptographic_materialproviders.mpl.references import IKeyring
+from typing import Dict  # noqa pylint: disable=wrong-import-order
+
+import aws_encryption_sdk
+from aws_encryption_sdk import CommitmentPolicy
+
+# TODO-MPL: Remove this as part of removing PYTHONPATH hacks.
+MODULE_ROOT_DIR = '/'.join(__file__.split("/")[:-1])
+
+sys.path.append(MODULE_ROOT_DIR)
+
+EXAMPLE_DATA: bytes = b"Hello World"
+
+
+def encrypt_and_decrypt_with_keyring(
+    default_region_kms_key_id: str,
+    second_region_kms_key_id: str,
+    default_region: str,
+    second_region: str
+):
+    """Demonstrate an encrypt/decrypt cycle using an AWS KMS Multi keyring.
+    The multi_keyring is created using a KMS keyring as generator keyring and another KMS keyring
+    as a child keyring. For this example, `default_region_kms_key_id` is the generator key id
+    for a KMS key located in your default region, and `second_region_kms_key_id` is the KMS key id
+    for a KMS Key located in some second region.
+
+    Usage: encrypt_and_decrypt_with_keyring(default_region_kms_key_id,
+                                            second_region_kms_key_id,
+                                            default_region,
+                                            second_region)
+    :param default_region_kms_key_id: KMS Key identifier for the default region KMS key you want to
+    use as a generator keyring
+    :type default_region_kms_key_id: string
+    :param second_region_kms_key_id: KMS Key identifier for the second region KMS key you want to
+    use as a child keyring
+    :type second_region_kms_key_id: string
+    :param default_region: AWS Region for the default region KMS key
+    :type default_region: string
+    :param second_region: AWS Region for the second region KMS key
+    :type second_region: string
+
+    For more information on KMS Key identifiers, see
+    https://docs.aws.amazon.com/kms/latest/developerguide/concepts.html#key-id
+    """
+    # 1. Instantiate the encryption SDK client.
+    # This builds the client with the REQUIRE_ENCRYPT_REQUIRE_DECRYPT commitment policy,
+    # which enforces that this client only encrypts using committing algorithm suites and enforces
+    # that this client will only decrypt encrypted messages that were created with a committing
+    # algorithm suite.
+    # This is the default commitment policy if you were to build the client as
+    # `client = aws_encryption_sdk.EncryptionSDKClient()`.
+    client = aws_encryption_sdk.EncryptionSDKClient(
+        commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
+    )
+
+    # 2. Create encryption context.
+    # Remember that your encryption context is NOT SECRET.
+    # For more information, see
+    # https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/concepts.html#encryption-context
+    encryption_context: Dict[str, str] = {
+        "encryption": "context",
+        "is not": "secret",
+        "but adds": "useful metadata",
+        "that can help you": "be confident that",
+        "the data you are handling": "is what you think it is",
+    }
+
+    # 3. Create an AwsKmsMultiKeyring that protects your data under two different KMS Keys.
+    # Either KMS Key individually is capable of decrypting data encrypted under this Multi Keyring.
+    mat_prov: AwsCryptographicMaterialProviders = AwsCryptographicMaterialProviders(
+        config=MaterialProvidersConfig()
+    )
+
+    kms_multi_keyring_input: CreateAwsKmsMultiKeyringInput = CreateAwsKmsMultiKeyringInput(
+        generator=default_region_kms_key_id,
+        kms_key_ids=[second_region_kms_key_id]
+    )
+
+    kms_multi_keyring: IKeyring = mat_prov.create_aws_kms_multi_keyring(
+        input=kms_multi_keyring_input
+    )
+
+    # 4. Encrypt the data with the encryptionContext
+    ciphertext, _ = client.encrypt(
+        source=EXAMPLE_DATA,
+        keyring=kms_multi_keyring,
+        encryption_context=encryption_context
+    )
+
+    # 5. Demonstrate that the ciphertext and plaintext are different.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert ciphertext != EXAMPLE_DATA, \
+        "Ciphertext and plaintext data are the same. Invalid encryption"
+
+    # 6a. Decrypt your encrypted data using the same multi_keyring you used on encrypt.
+    plaintext_bytes_multi_keyring, _ = client.decrypt(
+        source=ciphertext,
+        keyring=kms_multi_keyring
+    )
+
+    # 6b. Demonstrate that the decrypted plaintext is identical to the original plaintext.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert plaintext_bytes_multi_keyring == EXAMPLE_DATA, \
+        "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
+
+    # Because you used a multi_keyring on Encrypt, you can use either of the two
+    # kms keyrings individually to decrypt the data.
+
+    # 7. Demonstrate that you can successfully decrypt data using a KMS keyring with just the
+    # `default_region_kms_key_id` directly.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+
+    # 7a. Create a boto3 client for KMS for the default region.
+    default_region_kms_client = boto3.client('kms', region_name=default_region)
+
+    # 7b. Create KMS keyring
+    default_region_kms_keyring_input: CreateAwsKmsKeyringInput = CreateAwsKmsKeyringInput(
+        kms_key_id=default_region_kms_key_id,
+        kms_client=default_region_kms_client
+    )
+
+    default_region_kms_keyring: IKeyring = mat_prov.create_aws_kms_keyring(
+        input=default_region_kms_keyring_input
+    )
+
+    # 7c. Decrypt your encrypted data using the default_region_kms_keyring.
+    plaintext_bytes_default_region_kms_keyring, _ = client.decrypt(
+        source=ciphertext,
+        keyring=default_region_kms_keyring
+    )
+
+    # 7d. Demonstrate that the decrypted plaintext is identical to the original plaintext.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert plaintext_bytes_default_region_kms_keyring == EXAMPLE_DATA, \
+        "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
+
+    # 8. Demonstrate that you can also successfully decrypt data using a KMS keyring with just the
+    # `second_region_kms_key_id` directly.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+
+    # 8a. Create a boto3 client for KMS for the second region.
+    second_region_kms_client = boto3.client('kms', region_name=second_region)
+
+    # 8b. Create KMS keyring
+    second_region_kms_keyring_input: CreateAwsKmsKeyringInput = CreateAwsKmsKeyringInput(
+        kms_key_id=second_region_kms_key_id,
+        kms_client=second_region_kms_client
+    )
+
+    second_region_kms_keyring: IKeyring = mat_prov.create_aws_kms_keyring(
+        input=second_region_kms_keyring_input
+    )
+
+    # 8c. Decrypt your encrypted data using the second_region_kms_keyring.
+    plaintext_bytes_second_region_kms_keyring, _ = client.decrypt(
+        source=ciphertext,
+        keyring=second_region_kms_keyring
+    )
+
+    # 8d. Demonstrate that the decrypted plaintext is identical to the original plaintext.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert plaintext_bytes_second_region_kms_keyring == EXAMPLE_DATA, \
+        "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
---
+++ examples/src/file_streaming_example.py
@@ -0,0 +1,152 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+This example demonstrates file streaming for encryption and decryption.
+
+File streaming is useful when the plaintext or ciphertext file/data is too large to load into
+memory. Therefore, the AWS Encryption SDK allows users to stream the data, instead of loading it
+all at once in memory. In this example, we demonstrate file streaming for encryption and decryption
+using a Raw AES keyring. However, you can use any keyring with streaming.
+
+This example creates a Raw AES Keyring and then encrypts an input stream from the file
+`plaintext_filename` with an encryption context to an output (encrypted) file `ciphertext_filename`.
+It then decrypts the ciphertext from `ciphertext_filename` to a new file `decrypted_filename`.
+This example also includes some sanity checks for demonstration:
+1. Ciphertext and plaintext data are not the same
+2. Encryption context is correct in the decrypted message header
+3. Decrypted plaintext value matches EXAMPLE_DATA
+These sanity checks are for demonstration in the example only. You do not need these in your code.
+
+For more information on how to use Raw AES keyrings, see
+https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/use-raw-aes-keyring.html
+
+See raw_aes_keyring_example.py in the same directory for another raw AES keyring example
+in the AWS Encryption SDK for Python.
+"""
+import filecmp
+import secrets
+import sys
+
+from aws_cryptographic_materialproviders.mpl import AwsCryptographicMaterialProviders
+from aws_cryptographic_materialproviders.mpl.config import MaterialProvidersConfig
+from aws_cryptographic_materialproviders.mpl.models import AesWrappingAlg, CreateRawAesKeyringInput
+from aws_cryptographic_materialproviders.mpl.references import IKeyring
+from typing import Dict  # noqa pylint: disable=wrong-import-order
+
+import aws_encryption_sdk
+from aws_encryption_sdk import CommitmentPolicy
+
+# TODO-MPL: Remove this as part of removing PYTHONPATH hacks.
+MODULE_ROOT_DIR = '/'.join(__file__.split("/")[:-1])
+
+sys.path.append(MODULE_ROOT_DIR)
+
+
+def encrypt_and_decrypt_with_keyring(
+    plaintext_filename: str,
+    ciphertext_filename: str,
+    decrypted_filename: str
+):
+    """Demonstrate a streaming encrypt/decrypt cycle.
+
+    Usage: encrypt_and_decrypt_with_keyring(plaintext_filename
+                                            ciphertext_filename
+                                            decrypted_filename)
+    :param plaintext_filename: filename of the plaintext data
+    :type plaintext_filename: string
+    :param ciphertext_filename: filename of the ciphertext data
+    :type ciphertext_filename: string
+    :param decrypted_filename: filename of the decrypted data
+    :type decrypted_filename: string
+    """
+    # 1. Instantiate the encryption SDK client.
+    # This builds the client with the REQUIRE_ENCRYPT_REQUIRE_DECRYPT commitment policy,
+    # which enforces that this client only encrypts using committing algorithm suites and enforces
+    # that this client will only decrypt encrypted messages that were created with a committing
+    # algorithm suite.
+    # This is the default commitment policy if you were to build the client as
+    # `client = aws_encryption_sdk.EncryptionSDKClient()`.
+    client = aws_encryption_sdk.EncryptionSDKClient(
+        commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
+    )
+
+    # 2. The key namespace and key name are defined by you.
+    # and are used by the Raw AES keyring to determine
+    # whether it should attempt to decrypt an encrypted data key.
+    # For more information, see
+    # https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/use-raw-aes-keyring.html
+    key_name_space = "Some managed raw keys"
+    key_name = "My 256-bit AES wrapping key"
+
+    # 3. Create encryption context.
+    # Remember that your encryption context is NOT SECRET.
+    # For more information, see
+    # https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/concepts.html#encryption-context
+    encryption_context: Dict[str, str] = {
+        "encryption": "context",
+        "is not": "secret",
+        "but adds": "useful metadata",
+        "that can help you": "be confident that",
+        "the data you are handling": "is what you think it is",
+    }
+
+    # 4. Generate a 256-bit AES key to use with your keyring.
+    # In practice, you should get this key from a secure key management system such as an HSM.
+
+    # Here, the input to secrets.token_bytes() = 32 bytes = 256 bits
+    static_key = secrets.token_bytes(32)
+
+    # 5. Create a Raw AES keyring
+    # We choose to use a raw AES keyring, but any keyring can be used with streaming.
+    mat_prov: AwsCryptographicMaterialProviders = AwsCryptographicMaterialProviders(
+        config=MaterialProvidersConfig()
+    )
+
+    keyring_input: CreateRawAesKeyringInput = CreateRawAesKeyringInput(
+        key_namespace=key_name_space,
+        key_name=key_name,
+        wrapping_key=static_key,
+        wrapping_alg=AesWrappingAlg.ALG_AES256_GCM_IV12_TAG16
+    )
+
+    raw_aes_keyring: IKeyring = mat_prov.create_raw_aes_keyring(
+        input=keyring_input
+    )
+
+    # 6. Encrypt the data stream with the encryptionContext
+    with open(plaintext_filename, 'rb') as pt_file, open(ciphertext_filename, 'wb') as ct_file:
+        with client.stream(
+            mode='e',
+            source=pt_file,
+            keyring=raw_aes_keyring,
+            encryption_context=encryption_context
+        ) as encryptor:
+            for chunk in encryptor:
+                ct_file.write(chunk)
+
+    # 7. Demonstrate that the ciphertext and plaintext are different.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert not filecmp.cmp(plaintext_filename, ciphertext_filename), \
+        "Ciphertext and plaintext data are the same. Invalid encryption"
+
+    # 8. Decrypt your encrypted data stream using the same keyring you used on encrypt.
+    with open(ciphertext_filename, 'rb') as ct_file, open(decrypted_filename, 'wb') as pt_file:
+        with client.stream(
+            mode='d',
+            source=ct_file,
+            keyring=raw_aes_keyring,
+            encryption_context=encryption_context
+        ) as decryptor:
+            for chunk in decryptor:
+                pt_file.write(chunk)
+
+    # 9. Demonstrate that the encryption context is correct in the decrypted message header
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    for k, v in encryption_context.items():
+        assert v == decryptor.header.encryption_context[k], \
+            "Encryption context does not match expected values"
+
+    # 10. Demonstrate that the decrypted plaintext is identical to the original plaintext.
+    # (This is an example for demonstration; you do not need to do this in your own code.)
+    assert filecmp.cmp(plaintext_filename, decrypted_filename), \
+        "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
