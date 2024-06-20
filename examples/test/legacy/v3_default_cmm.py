7bbe83e24a2b4ef0822c82c183cc7a9143bb0267
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-06-20 10:08:07 -0700

    fix

--- examples/test/legacy/v3_default_cmm.py
+++ examples/test/legacy/v3_default_cmm.py
@@ -1,6 +1,7 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Copy-paste of the V3 default CMM."""
+# flake8: ignore=D205,D400,D401
 import logging
 
 import attr
