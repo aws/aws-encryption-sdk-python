3b5157d40d1ce60d98b056ef395d1793e0895fb0
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-06-27 14:56:48 -0700

    add

---
+++ examples/src/multithreading/__init__.py
@@ -0,0 +1,62 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""init file for multi-threading examples."""
+from aws_cryptographic_materialproviders.mpl.references import IKeyring
+from typing import Dict  # noqa pylint: disable=wrong-import-order
+
+import aws_encryption_sdk
+import time
+
+
+def encrypt_and_decrypt_with_keyring(
+    plaintext_data: bytes,
+    keyring: IKeyring,
+    client: aws_encryption_sdk.EncryptionSDKClient
+):
+    """Demonstrate how to encrypt and decrypt plaintext data using a keyring.
+
+    Usage: encrypt_and_decrypt_with_keyring(plaintext_data, keyring, client)
+    :param plaintext_data: plaintext data you want to encrypt
+    :type: bytes
+    :param keyring: Keyring to use for encryption.
+    :type keyring: IKeyring
+    :param client: The Encryption SDK client to use for encryption.
+    :type client: aws_encryption_sdk.EncryptionSDKClient
+    :return: encrypted and decrypted (cycled) plaintext data
+    :rtype: bytes
+    """
+    encryption_context: Dict[str, str] = {
+        "encryption": "context",
+        "is not": "secret",
+        "but adds": "useful metadata",
+        "that can help you": "be confident that",
+        "the data you are handling": "is what you think it is",
+    }
+
+    ciphertext_data, _ = client.encrypt(
+        source=plaintext_data,
+        keyring=keyring,
+        encryption_context=encryption_context
+    )
+
+    decrypted_plaintext_data, _ = client.decrypt(
+        source=ciphertext_data,
+        keyring=keyring
+    )
+
+    return decrypted_plaintext_data
+
+
+def run_encrypt_and_decrypt_with_keyring_for_duration_seconds(
+    plaintext_data: bytes,
+    keyring: IKeyring,
+    client: aws_encryption_sdk.EncryptionSDKClient,
+    duration: int = 2
+):
+    """Helper function to repeatedly run an encrypt and decrypt cycle for 'duration' seconds."""
+    time_end = time.time() + duration
+
+    while time.time() < time_end:
+        decrypted_plaintext_data = encrypt_and_decrypt_with_keyring(plaintext_data, keyring, client)
+        assert decrypted_plaintext_data == plaintext_data, \
+            "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
---
+++ examples/test/multithreading/test_i_raw_aes_keyring_multithreaded_example.py
@@ -0,0 +1,50 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""Test suite for the Raw AES keyring example with multi-threading."""
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import pytest
+
+import aws_encryption_sdk
+from aws_encryption_sdk import CommitmentPolicy
+
+from ...src.multithreading import run_encrypt_and_decrypt_with_keyring_for_duration_seconds
+from ...src.multithreading.raw_aes_keyring import create_keyring
+
+import time
+
+pytestmark = [pytest.mark.examples]
+
+
+def test_encrypt_and_decrypt_with_keyring_multithreaded_helper(n_threads=16, duration=60):
+    """Helper function for multi-threaded encrypt and decrypt using a keyring for fixed n_threads and duration."""
+    print(n_threads, duration)
+    start_time = time.time()
+    print('start_time', start_time)
+    keyring = create_keyring()
+    plaintext_data = b"Hello World"
+    client = aws_encryption_sdk.EncryptionSDKClient(
+        commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
+    )
+
+    with ThreadPoolExecutor(max_workers=n_threads) as executor:
+        thread_futures = {executor.submit(run_encrypt_and_decrypt_with_keyring_for_duration_seconds,
+                                          plaintext_data=plaintext_data,
+                                          keyring=keyring,
+                                          client=client,
+                                          duration=duration): i for i in range(n_threads)}
+
+        for future in as_completed(thread_futures):
+            future.result()
+    end_time = time.time()
+    print('end_time', end_time)
+    print('duration', end_time - start_time)
+
+
+# def test_encrypt_and_decrypt_with_keyring_multithreaded(n_threads_list: list = [4, 16, 64], duration_list: list = [2, 10, 60]):
+#     """Test function for multi-threaded encrypt and decrypt using a keyring for different n_threads and duration."""
+#     print('hello', n_threads_list, duration_list)
+#     for n in n_threads_list:
+#         for d in duration_list:
+#             print(n, d, time.time())
+#             encrypt_and_decrypt_with_keyring_helper(n_threads=n, duration=d)
