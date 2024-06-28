1591bdf4b3c0ca9232d24bd71aea74d77859351d
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-06-27 17:04:07 -0700

    refactoring and fixes

--- examples/test/multithreading/test_i_raw_rsa_keyring_multithreaded_example.py
+++ examples/test/multithreading/test_i_raw_rsa_keyring_multithreaded_example.py
@@ -8,14 +8,14 @@ import pytest
 import aws_encryption_sdk
 from aws_encryption_sdk import CommitmentPolicy
 
-from ...src.multithreading import encrypt_and_decrypt_with_keyring
+from ...src.multithreading import run_encrypt_and_decrypt_with_keyring_for_duration_seconds
 from ...src.multithreading.raw_rsa_keyring import create_keyring, generate_rsa_keys
 
 pytestmark = [pytest.mark.examples]
 
 
-def test_encrypt_and_decrypt_with_keyring(n_threads=10):
-    """Test function for multi-threaded encrypt and decrypt using the Raw RSA Keyring example."""
+def encrypt_and_decrypt_with_keyring_multithreaded_helper(n_threads=64, duration=60):
+    """Helper function for multi-threaded encrypt and decrypt using a keyring for fixed n_threads and duration."""
     public_key, private_key = generate_rsa_keys()
     keyring = create_keyring(public_key=public_key, private_key=private_key)
     plaintext_data = b"Hello World"
@@ -24,12 +24,18 @@ def test_encrypt_and_decrypt_with_keyring(n_threads=10):
     )
 
     with ThreadPoolExecutor(max_workers=n_threads) as executor:
-        thread_futures = {executor.submit(encrypt_and_decrypt_with_keyring,
+        thread_futures = {executor.submit(run_encrypt_and_decrypt_with_keyring_for_duration_seconds,
                                           plaintext_data=plaintext_data,
                                           keyring=keyring,
-                                          client=client): i for i in range(n_threads)}
+                                          client=client,
+                                          duration=duration): i for i in range(n_threads)}
 
         for future in as_completed(thread_futures):
-            decrypted_plaintext_data = future.result()
-            assert decrypted_plaintext_data == plaintext_data, \
-                "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
+            future.result()
+
+
+def test_encrypt_and_decrypt_with_keyring_multithreaded(n_threads_list: list = [1, 4, 16, 64], duration_list: list = [2, 10, 60]):
+    """Test function for multi-threaded encrypt and decrypt using a keyring for different n_threads and duration."""
+    for n in n_threads_list:
+        for d in duration_list:
+            encrypt_and_decrypt_with_keyring_multithreaded_helper(n_threads=n, duration=d)
