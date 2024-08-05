9085f08aea507a78512818cd36a6a3c439936f61
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-08-05 10:53:16 -0700

    fix

--- examples/src/default_cryptographic_materials_manager_example.py
+++ examples/src/default_cryptographic_materials_manager_example.py
@@ -18,8 +18,6 @@ These sanity checks are for demonstration in the example only. You do not need t
 For more information on Cryptographic Material Managers, see
 https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/concepts.html#crypt-materials-manager
 """
-import sys
-
 import boto3
 from aws_cryptographic_materialproviders.mpl import AwsCryptographicMaterialProviders
 from aws_cryptographic_materialproviders.mpl.config import MaterialProvidersConfig
@@ -32,11 +30,6 @@ from typing import Dict  # noqa pylint: disable=wrong-import-order
 
 import aws_encryption_sdk
 from aws_encryption_sdk import CommitmentPolicy
-
-# TODO-MPL: Remove this as part of removing PYTHONPATH hacks.
-MODULE_ROOT_DIR = '/'.join(__file__.split("/")[:-1])
-
-sys.path.append(MODULE_ROOT_DIR)
 
 EXAMPLE_DATA: bytes = b"Hello World"
 
