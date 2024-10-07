4e6a532556bf11ae6348aae6e9d39e7891ddee3f
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-10-07 15:27:43 -0700

    fix

--- examples/src/aws_kms_discovery_keyring_example.py
+++ examples/src/aws_kms_discovery_keyring_example.py
@@ -164,8 +164,8 @@ def encrypt_and_decrypt_with_keyring(
     assert plaintext_bytes == EXAMPLE_DATA, \
         "Decrypted plaintext should be identical to the original plaintext. Invalid decryption"
 
-    # 10. Demonstrate that if a discovery keyring (Bob's) doesn't have the correct AWS Account ID's,
-    # the decrypt will fail with an error message
+    # 10. Demonstrate that if a different discovery keyring (Bob's) doesn't have the correct
+    # AWS Account ID's, the decrypt will fail with an error message
     # Note that this assumes Account ID used here ('888888888888') is different than the one used
     # during encryption
     discovery_keyring_input_bob: CreateAwsKmsDiscoveryKeyringInput = \
