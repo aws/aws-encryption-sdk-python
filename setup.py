8bcc834c90e35dc06378a872ea4204cb5d7dd207
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-10-17 14:21:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # TODO-MPL: Point at PyPI once MPL is released.
     # This blocks releasing ESDK-Python MPL integration.
     extras_require={
-        "MPL": ["aws-cryptographic-material-providers @" \ 
+        "MPL": ["aws-cryptographic-material-providers @" \
                 "git+https://github.com/aws/aws-cryptographic-material-providers-library.git@" \
                 "python-main-with-dafny-code#subdirectory=AwsCryptographicMaterialProviders/runtimes/python"],
     },
