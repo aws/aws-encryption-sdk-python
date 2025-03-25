93b023a3e37e032b1c8d1cbb4a61e4176b454c84
Lucas McDonald <lucmcdon@amazon.com>
2025-03-25 13:36:57 -0700

    update expr

--- test_vector_handlers/scripts/install_mpl_test_vector_runner.sh
+++ test_vector_handlers/scripts/install_mpl_test_vector_runner.sh
@@ -7,8 +7,10 @@
 # Change to the directory of the script
 cd "$(dirname "$0")"
 
-# Get MPL version specified in requirements.txt
-export mplVersion=$(grep 'aws-cryptographic-material-providers==' ../requirements_mpl.txt | sed -E 's/.*==(.+)/\1/')
+# Get highest MPL version specified in requirements.txt
+# export mplVersion=$(grep 'aws-cryptographic-material-providers==' ../requirements_mpl.txt | sed -E 's/.*==//' | tr ',' '\n' | sed -E 's/^[^0-9]*//' | sort -V | tail -n1)
+# Get lowest MPL version specified in requirements.txt
+export mplVersion=$(grep 'aws-cryptographic-material-providers==' ../requirements_mpl.txt | sed -E 's/.*==//' | tr ',' '\n' | sed -E 's/^[^0-9]*//' | sort -V | head -n1)
 
 # Clone MPL repo to get test vectors runner source code and the Dafny version to use
 git clone --branch v$mplVersion --recurse-submodules https://github.com/aws/aws-cryptographic-material-providers-library.git
